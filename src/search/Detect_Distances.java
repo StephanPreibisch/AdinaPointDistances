a89fe9949071e2ce919844cad3c8c9cbbfa266d9
Stephan Preibisch <stephan.preibisch@gmx.de>
2014-03-17 16:53:33 -0400

    added bi-directional search for mask/spots

--- src/search/Detect_Distances.java
+++ src/search/Detect_Distances.java
@@ -22,7 +22,8 @@ import net.imglib2.type.numeric.real.FloatType;
 
 public class Detect_Distances implements PlugIn
 {
-	public static String outputfile = "1_6.new.loc";
+	public static String outputfilePTM = "1_6.points_to_mask.loc";
+	public static String outputfileMTP = "1_6.mask_to_points.loc";
 	public static String textfile = "1_6.loc";
 	public static String imagefile = "C1-1_6-mask.tif";
 
@@ -33,7 +34,8 @@ public class Detect_Distances implements PlugIn
 		
 		gd.addFileField( "Segmented Image (bg = 0, fg > 1)", imagefile, 50 );
 		gd.addFileField( "RNA's (textfile, multicolumn)", textfile, 50 );
-		gd.addFileField( "Outputfile", outputfile, 50 );
+		gd.addFileField( "Outputfile (points to mask)", outputfilePTM, 50 );
+		gd.addFileField( "Outputfile (mask to points)", outputfileMTP, 50 );
 		
 		gd.showDialog();
 		
@@ -42,7 +44,8 @@ public class Detect_Distances implements PlugIn
 		
 		imagefile = gd.getNextString();
 		textfile = gd.getNextString();
-		outputfile = gd.getNextString();
+		outputfilePTM = gd.getNextString();
+		outputfileMTP = gd.getNextString();
 
 		if ( !new File( imagefile ).exists() )
 		{
@@ -58,8 +61,7 @@ public class Detect_Distances implements PlugIn
 		
 		final Img< FloatType > img = ImagePlusAdapter.convertFloat( new ImagePlus( imagefile ) );
 		
-		final ArrayList< RealPoint > pixelsBigger0 = new ArrayList<RealPoint>();
-		
+		final ArrayList< RealPoint > pixelsBigger0 = new ArrayList< RealPoint >();
 		final Cursor< FloatType > cursor = img.localizingCursor();
 		
 		while ( cursor.hasNext() )
@@ -69,13 +71,10 @@ public class Detect_Distances implements PlugIn
 			if ( cursor.get().get() > 0 )
 				pixelsBigger0.add( new RealPoint( cursor ) );
 		}
-		
-		final KDTree< RealPoint > kdTree = new KDTree< RealPoint >( pixelsBigger0, pixelsBigger0 );
-		final NearestNeighborSearchOnKDTree< RealPoint > search = new NearestNeighborSearchOnKDTree<RealPoint>( kdTree );
-		
+				
 		final BufferedReader in = TextFileAccess.openFileRead( textfile );
-		final PrintWriter out = TextFileAccess.openFileWrite( outputfile );
-		
+		final ArrayList< RealPoint > spots = new ArrayList<RealPoint>();
+
 		try 
 		{
 			final double[] tmp = new double[ 2 ];
@@ -97,15 +96,42 @@ public class Detect_Distances implements PlugIn
 				
 				tmp[ 0 ] = Double.parseDouble( entries[ 0 ] ) - 0.5;
 				tmp[ 1 ] = Double.parseDouble( entries[ 1 ] ) - 0.5;
-				
-				final RealPoint p = new RealPoint( tmp );
-				search.search( p );
-				final double distance = search.getDistance();
-				
-				out.println( entries[ 0 ] + "\t" + entries[ 1 ] + "\t" + distance );
+
+				spots.add( new RealPoint( tmp[ 0 ], tmp[ 1 ] ) );
 			}
 			
 			in.close();
+
+			final KDTree< RealPoint > kdTreeMask = new KDTree< RealPoint >( pixelsBigger0, pixelsBigger0 );
+			final NearestNeighborSearchOnKDTree< RealPoint > searchMask = new NearestNeighborSearchOnKDTree<RealPoint>( kdTreeMask );
+
+			// write out result	outputfilePTM
+			PrintWriter out = TextFileAccess.openFileWrite( outputfilePTM );
+
+			for ( final RealPoint point : spots )
+			{
+				searchMask.search( point );
+				final double distance = searchMask.getDistance();
+
+				out.println( (point.getDoublePosition( 0 ) + 0.5) + "\t" + (point.getDoublePosition( 1 ) + 0.5) + "\t" + distance );
+			}
+			
+			out.close();
+
+			final KDTree< RealPoint > kdTreeSpots = new KDTree< RealPoint >( spots, spots );
+			final NearestNeighborSearchOnKDTree< RealPoint > searchSpots = new NearestNeighborSearchOnKDTree<RealPoint>( kdTreeSpots );
+
+			// write out result	outputfilePTM
+			out = TextFileAccess.openFileWrite( outputfileMTP );
+
+			for ( final RealPoint maskPoint : pixelsBigger0 )
+			{
+				searchSpots.search( maskPoint );
+				final double distance = searchSpots.getDistance();
+
+				out.println( (maskPoint.getDoublePosition( 0 )) + "\t" + (maskPoint.getDoublePosition( 1 )) + "\t" + distance );
+			}
+			
 			out.close();
 			
 			IJ.log( "output written." );
